21feac5cd3a04e0e52307ca8f70ac481bb215648
akakh18 <akakh18@freeuni.edu.ge>
2022-07-26 22:01:08 +0400

    Create GeorgianWord2Vec class in utils.py

--- utils.py
+++ utils.py
@@ -10,21 +10,29 @@ CHECK_NON_EMPTY_FN = lambda s: len(s) > 0
 
 class GeorgianWord2Vec:
     def __init__(self) -> None:
-        self.model = Word2Vec(sentences=INIT_DATA, vector_size=100, window=5, min_count=1, workers=4)
+        print("Initializing data")
+        self.__model_name = "word2vec.model"
+        model = Word2Vec(sentences=INIT_DATA, vector_size=100, window=5, min_count=1, workers=4, epochs=3)
+        model.save(self.__model_name)
         self.sentences_split_regex = "\. |!|\?"
 
     # Returns word2vec model, trained by initial dataset
     def get_model(self) -> Word2Vec:
-        return self.model
+        return Word2Vec.load(self.__model_name)
 
     # Gets the file path and trains the model using its sentences
     def train(self, file_path: str) -> None:
+        model = self.get_model()
         sentences = self.__convert_file_into_input(file_path)
-        self.model.train(sentences, self.model.corpus_count, self.model.epochs)
+        print(sentences[0])
+        model.build_vocab(sentences, update=True)
+        model.train(sentences, total_examples=model.corpus_count,
+                    epochs=model.epochs)
+        model.save(self.__model_name)
 
     # Returns vector corresponding to passed word
     def get_vector(self, word: str) -> List[int]:
-        return self.model.wv[word]
+        return Word2Vec.load(self.__model_name).wv[word]
 
     def __convert_file_into_input(self, file_path: str) -> List[List[str]]:
         with open(file_path, 'r') as f:
