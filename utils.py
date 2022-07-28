bffb878c9d3138f2e4c5c1eb9752dae0f19cb280
akakh18 <akakh18@freeuni.edu.ge>
2022-07-28 22:17:18 +0400

    Add some changes

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
-from typing import List
+from typing import Protocol, List
 
-import nltk
-from gensim.models import Word2Vec
+from gensim.models import Word2Vec, FastText
 from nltk.tokenize import sent_tokenize
 
 INIT_DATA = [["გამარჯობა", "როგორ", "ხარ?"], ["რავი", "კარგად", "შენ?"]]
@@ -9,38 +8,79 @@ INIT_DATA = [["გამარჯობა", "როგორ", "ხარ?"], [
 CHECK_NON_EMPTY_FN = lambda s: len(s) > 0
 
 
-class GeorgianWord2Vec:
+def strip_word(word: str) -> str:
+    r_strip_chars = ['.', ',', '!', '?', ':', ';']
+
+    result = word
+
+    for c in r_strip_chars:
+        result = result.rstrip(c)
+
+    return result.strip().strip("(").strip(")")
+
+
+def convert_file_into_input(file_path: str) -> List[List[str]]:
+    with open(file_path, 'r') as f:
+        data = f.read()
+
+    sentences = sent_tokenize(data)
+    return [[strip_word(word) for word in sentence.split(" ")] for sentence in sentences]
+
+
+class GeorgianModel(Protocol):
+    def get_model(self) -> Word2Vec:
+        """Returns word2vec model, trained by initial dataset"""
+
+    def train(self, file_path: str) -> None:
+        """Gets the file path and trains the model using its sentences"""
+
+    def get_vector(self, word: str) -> List[int]:
+        """Returns vector corresponding to passed word"""
+
+
+class GeorgianWord2VecModel:
     def __init__(self) -> None:
         print("Initializing data")
-        nltk.download('punkt')
         self.__model_name = "word2vec.model"
         model = Word2Vec(sentences=INIT_DATA, vector_size=100, window=5, min_count=1, workers=4, epochs=3)
         model.save(self.__model_name)
         print("Model created!")
 
-    # Returns word2vec model, trained by initial dataset
     def get_model(self) -> Word2Vec:
         return Word2Vec.load(self.__model_name)
 
-    # Gets the file path and trains the model using its sentences
     def train(self, file_path: str) -> None:
         model = self.get_model()
-        sentences = self.__convert_file_into_input(file_path)
-        print(sentences[0])
+        sentences = convert_file_into_input(file_path)
         model.build_vocab(sentences, update=True)
-        model.train(sentences, total_examples=model.corpus_count,
-                    epochs=model.epochs)
+        model.train(sentences, total_examples=model.corpus_count, epochs=model.epochs)
         model.save(self.__model_name)
 
-    # Returns vector corresponding to passed word
     def get_vector(self, word: str) -> List[int]:
-        return Word2Vec.load(self.__model_name).wv[word]
+        return Word2Vec.load(self.__model_name).wv.get_vector(word)
 
-    @staticmethod
-    def __convert_file_into_input(file_path: str) -> List[List[str]]:
-        with open(file_path, 'r') as f:
-            data = f.read()
 
-        sentences = sent_tokenize(data)
-        print(sentences)
-        return [[word.strip() for word in sentence.split(" ")] for sentence in sentences]
+class GeorgianFastTextModel:
+    def __init__(self) -> None:
+        print("Initializing data")
+        self.__model_name = "fasttext.model"
+        model = FastText(sentences=INIT_DATA, vector_size=100, window=5, min_count=1, workers=4, epochs=3)
+        model.save(self.__model_name)
+        print("Model created!")
+
+    def get_model(self) -> FastText:
+        return FastText.load(self.__model_name)
+
+    def train(self, file_path: str) -> None:
+        model = self.get_model()
+        sentences = convert_file_into_input(file_path)
+        model.build_vocab(sentences, update=True)
+        model.train(sentences, total_examples=model.corpus_count, epochs=model.epochs)
+        model.save(self.__model_name)
+
+    def get_vector(self, word: str) -> List[int]:
+        return FastText.load(self.__model_name).wv.get_vector(word)
+
+
+if __name__ == "__main__":
+    print(convert_file_into_input("data/data2.txt"))
